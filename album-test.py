75c02afbd124d25b1488e6824a6099c1c8724f67
splconline <63439754+splconline@users.noreply.github.com>
2023-10-26 08:12:01 +0800

    Update album-test.py

--- album-test.py
+++ album-test.py
@@ -1,47 +1,27 @@
 import requests, bs4
 
 # Open the URL
+# (Tested on Beatles, Queen, Miles Davis (Columbia only))
 exfile = requests.get("https://en.wikipedia.org/wiki/The_Beatles_discography")
 exfile.raise_for_status()
 
 # Soup it
 exsoup = bs4.BeautifulSoup(exfile.text,'html.parser')
 
-# Find the *first* album listing table (which will be the UK albums)
+# Find the *first* album listing table (which e.g. for the Beatles wikipedia page will
+# be the UK albums, for Miles Davis will be the Columbia albums etc.)
 albums = exsoup.find("table", {"class": "wikitable plainrowheaders"})
 
-# Extract the data
-data = []
-for row in albums.find_all('tr'):
-    row_data = []
-    for cell in row.contents:
-        row_data.append(cell.text)
-    data.append(row_data)
+# Extracting the data we found that for class 'wikitable plainrowheaders' in an artist page:
+#
+# * Listing elements 1,2,3, n-1 and n shows that album names are in element 3 to n-1
+# * The lbum title is in the second index
+#
+# Therefore, the following gets the URLs of all albums:
 
-dataLength = len(data)
-print('Table length:' + str(dataLength))
+rows = albums.find_all('tr')
+n = len(rows)
 
-print('First element:')
-print(data[0])
-
-print('Second element:')
-print(data[1])
-
-print('Third element:')
-print(data[2])
-
-print('Penultimate element:')
-print(data[dataLength-2])
-
-print('Last element:')
-print(data[dataLength-1])
-
-print('Third Element, Second Index:')
-print(data[2][1])
-
-print('Fourth Element, Second Index:')
-print(data[3][1])
-
-print('Fifth Element, Second Index:')
-print(data[4][1])
-
+for i in range(2, n-1, 1):
+    url = rows[i].find('a', href=True)
+    print(url['href'])
